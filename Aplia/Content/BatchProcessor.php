b0e5bb80967f706dc0fe079c5fe5b4fa36ff5704
Jan Borsodi <jb@aplia.no>
2018-09-17 13:02:06 +0200

    Added a batch processor class for easily modifying multiple objects
    It takes a QuerySet object as parameter (or parameters for QuerySet)
    and takes care of iterating over all objects in that set and calling
    a callback. This class can either be used directly by passing
    functions into the callbacks or by subclassing.
    The processor defaults to iterating over objects (ie. main nodes) and
    will pass a ContentObject instance to the callback, however it may be
    changed to visiting nodes instead.
    There are also callbacks for when an object was modified, skipped or
    one for all visited nodes/objects.

---
+++ Aplia/Content/BatchProcessor.php
@@ -0,0 +1,127 @@
+<?php
+namespace Aplia\Content;
+use Aplia\Support\Arr;
+use Aplia\Content\Query\QuerySet;
+use Aplia\Content\ContentObject;
+use eZContentObjectTreeNode;
+
+class BatchProcessor
+{
+    public $globalLimits = false;
+    public $objectMode = true;
+    public $modifiedCount = 0;
+    // Callbacks
+    public $processCallback;
+    public $completedCallback;
+    public $skippedCallback;
+    public $visitedCallback;
+
+    public function __construct($params=null)
+    {
+        $queryParams = null;
+        if (is_array($params)) {
+            $this->globalLimits = Arr::get($params, 'globalLimits', false);
+            $query = Arr::get($params, 'query');
+            if (is_array($query)) {
+                $queryParams = $query;
+                $query = null;
+            }
+            $this->processCallback = Arr::get($params, 'process');
+            $this->completedCallback = Arr::get($params, 'completed');
+            $this->skippedCallback = Arr::get($params, 'skipped');
+            $this->visitedCallback = Arr::get($params, 'visited');
+            $this->objectMode = Arr::get($params, 'objectMode', true);
+        } else {
+            $query = $params;
+        }
+        if (!$this->globalLimits) {
+            $defaultParams = array(
+                'useVisibility' => false,
+                'useRoles' => false,
+            );
+            // When using object mode the default should be to only visit nodes once
+            if ($this->objectMode) {
+                $defaultParams['mainNodeOnly'] = true;
+            }
+            if ($queryParams === null) {
+                $queryParams = $defaultParams;
+            } else {
+                $queryParams = array_merge(
+                    $defaultParams, $queryParams
+                );
+            }
+        }
+        if (!$query) {
+            $query = new QuerySet($queryParams);
+        } else if (!($query instanceof QuerySet)) {
+            throw new TypeError("Parameter to " . get_class($this) . " must either be an array or a Aplia\Content\Query\QuerySet instance");
+        }
+        $this->query = $query;
+    }
+
+    public function process()
+    {
+        if ($this->objectMode) {
+            foreach ($this->query as $node) {
+                if ($this->processObject($node)) {
+                    $this->modifiedCount += 1;
+                    $this->onCompleted($node);
+                } else {
+                    $this->onSkipped($node);
+                }
+                $this->onVisited($node);
+            }
+        } else {
+            foreach ($this->query as $node) {
+                if ($this->processNode($node)) {
+                    $this->modifiedCount += 1;
+                    $this->onCompleted($node);
+                } else {
+                    $this->onSkipped($node);
+                }
+                $this->onVisited($node);
+            }
+        }
+    }
+
+    public function processNode(eZContentObjectTreeNode $node)
+    {
+        if ($this->processCallback) {
+            return call_user_func($this->processCallback, $node);
+        }
+        return false;
+    }
+
+    public function processObject(eZContentObjectTreeNode $node)
+    {
+        $contentObject = $node->object();
+        $object = new ContentObject(array(
+            'contentObject' => $contentObject,
+        ));
+        if ($this->processCallback) {
+            return call_user_func($this->processCallback, $object);
+        }
+        return false;
+    }
+
+    public function onCompleted(eZContentObjectTreeNode $node)
+    {
+        if ($this->completedCallback) {
+            return call_user_func($this->completedCallback, $node);
+        }
+    }
+
+    public function onSkipped(eZContentObjectTreeNode $node)
+    {
+        if ($this->skippedCallback) {
+            return call_user_func($this->skippedCallback, $node);
+        }
+    }
+
+    public function onVisited(eZContentObjectTreeNode $node)
+    {
+        if ($this->visitedCallback) {
+            return call_user_func($this->visitedCallback, $node);
+        }
+    }
+}
