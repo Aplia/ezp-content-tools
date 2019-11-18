745b99102ee9ed82fb2ab0ee7813f71d5456654d
Jørgen Solli <js@aplia.no>
2019-11-18 10:02:24 +0100

    Implemented missing method for validating file

--- Aplia/Content/HttpFile.php
+++ Aplia/Content/HttpFile.php
@@ -53,5 +53,27 @@ class HttpFile
 
     protected function validate()
     {
+        $canFetchFile = eZHTTPFile::canFetch($this->name, $this->maxSize);
+        if (!$canFetchFile) {
+            if (!$this->isRequired) {
+                return;
+            }
+        } else {
+            $this->hasFile = true;
+        }
+        if ($this->isRequired && $canFetchFile === eZHTTPFile::UPLOADEDFILE_DOES_NOT_EXIST) {
+            $this->errors[] = ezpI18n::tr( 'kernel/classes/datatypes',
+                'A valid file file is required.' );
+        }
+        if ($canFetchFile === eZHTTPFile::UPLOADEDFILE_EXCEEDS_PHP_LIMIT) {
+            $this->errors[] = ezpI18n::tr( 'kernel/classes/datatypes',
+                'The size of the uploaded file exceeds limit set by upload_max_filesize directive in php.ini. Please contact the site administrator.' );
+        }
+        if ($canFetchFile === eZHTTPFile::UPLOADEDFILE_EXCEEDS_MAX_SIZE) {
+            $this->errors[] = ezpI18n::tr( 'kernel/classes/datatypes',
+                'The size of the uploaded file exceeds the limit set for this site: %1 bytes.', null, array('%1' => $this->maxSize));
+        }
+
+        $this->isValid = count($this->errors) == 0 && (bool)$canFetchFile;
     }
 }
